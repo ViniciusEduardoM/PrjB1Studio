dd96cd85c3d66ef0bb643bb0990a226bec6946f7
Vinicius Chichaveke <viniciuseduardo.info@gmail.com>
2022-12-07 09:48:22 -0300

    Fix: Matrix With Data

--- Form1.b1f.cs
+++ Form1.b1f.cs
@@ -20,17 +20,17 @@ namespace SAPExemple
         /// </summary>
         public override void OnInitializeComponent()
         {
-            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("lbCardCode").Specific));
-            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("tCardCode").Specific));
-            this.EditText0.ChooseFromListAfter += new SAPbouiCOM._IEditTextEvents_ChooseFromListAfterEventHandler(this.EditText0_ChooseFromListAfter);
+            this.lbCardCode = ((SAPbouiCOM.StaticText)(this.GetItem("lbCardCode").Specific));
+            this.txtCardCode = ((SAPbouiCOM.EditText)(this.GetItem("tCardCode").Specific));
+            this.txtCardCode.ChooseFromListAfter += new SAPbouiCOM._IEditTextEvents_ChooseFromListAfterEventHandler(this.EditText0_ChooseFromListAfter);
             this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
             this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("tCardName").Specific));
             this.EditText1.ChooseFromListAfter += new SAPbouiCOM._IEditTextEvents_ChooseFromListAfterEventHandler(this.EditText1_ChooseFromListAfter);
-            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("btnSearch").Specific));
-            this.Button0.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button0_ClickBefore);
-            this.Grid0 = ((SAPbouiCOM.Grid)(this.GetItem("grdOPCH").Specific));
-            this.Grid1 = ((SAPbouiCOM.Grid)(this.GetItem("grdMOPCH").Specific));
-            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_8").Specific));
+            this.btnSearch = ((SAPbouiCOM.Button)(this.GetItem("btnSearch").Specific));
+            this.btnSearch.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button0_ClickBefore);
+            this.gridQuery = ((SAPbouiCOM.Grid)(this.GetItem("grdOPCH").Specific));
+            this.gridManual = ((SAPbouiCOM.Grid)(this.GetItem("grdMOPCH").Specific));
+            this.oMatrix = ((SAPbouiCOM.Matrix)(this.GetItem("Item_8").Specific));
             this.StaticText2 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
             this.StaticText3 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
             this.StaticText4 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_3").Specific));
@@ -38,15 +38,15 @@ namespace SAPExemple
             this.EditText3 = ((SAPbouiCOM.EditText)(this.GetItem("Item_5").Specific));
             this.StaticText5 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_6").Specific));
             this.StaticText6 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_7").Specific));
-            this.OptionBtn0 = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_12").Specific));
-            this.OptionBtn0.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn0_ClickBefore);
-            this.OptionBtn1 = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_13").Specific));
-            this.OptionBtn1.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn1_ClickBefore);
-            this.OptionBtn2 = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_14").Specific));
-            this.OptionBtn2.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn2_ClickBefore);
-            this.OptionBtn1.GroupWith("Item_12");
-            this.OptionBtn2.GroupWith("Item_12");
-            this.PictureBox1 = ((SAPbouiCOM.PictureBox)(this.GetItem("Item_15").Specific));
+            this.oOptionButtonN = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_12").Specific));
+            this.oOptionButtonN.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn0_ClickBefore);
+            this.oOptionButtonP = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_13").Specific));
+            this.oOptionButtonP.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn1_ClickBefore);
+            this.oOptionButtonDate = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_14").Specific));
+            this.oOptionButtonDate.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn2_ClickBefore);
+            this.oOptionButtonP.GroupWith("Item_12");
+            this.oOptionButtonDate.GroupWith("Item_12");
+            this.PictureLAB = ((SAPbouiCOM.PictureBox)(this.GetItem("Item_15").Specific));
             this.StaticText7 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_9").Specific));
             this.OnCustomInitialize();
 
@@ -61,22 +61,39 @@ namespace SAPExemple
 
         }
 
-        private SAPbouiCOM.StaticText StaticText0;
-
         private void OnCustomInitialize()
         {
-            OptionBtn0.Selected = true;
+            oOptionButtonN.Selected = true;
         }
 
-        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.StaticText lbCardCode;
+        private SAPbouiCOM.EditText txtCardCode;
         private SAPbouiCOM.StaticText StaticText1;
         private SAPbouiCOM.EditText EditText1;
-        private SAPbouiCOM.Button Button0;
-        private SAPbouiCOM.Grid Grid0;
-        private SAPbouiCOM.Grid Grid1;
+        private SAPbouiCOM.Button btnSearch;
+        private SAPbouiCOM.Grid gridQuery;
+        private SAPbouiCOM.Grid gridManual;
+
+        private StaticText StaticText2;
+        private StaticText StaticText3;
+        private StaticText StaticText4;
+        private EditText EditText2;
+        private EditText EditText3;
+        private StaticText StaticText5;
+        private StaticText StaticText6;
+        private OptionBtn oOptionButtonN;
+        private OptionBtn oOptionButtonP;
+        private OptionBtn oOptionButtonDate;
+
+        private PictureBox PictureLAB;
+        private StaticText StaticText7;
 
         private SAPbouiCOM.DataTable dt_OCRD;
+        private SAPbouiCOM.DataTable dt_Matrix;
+        private SAPbouiCOM.DataTable dt_Status;
 
+
+        private SAPbouiCOM.Matrix oMatrix;
 
         private Form oForm;
 
@@ -84,14 +101,17 @@ namespace SAPExemple
         {
             BubbleEvent = true;
 
+            dt_Matrix = UIAPIRawForm.DataSources.DataTables.Item("DS_Matrix");
+            dt_Status = UIAPIRawForm.DataSources.DataTables.Item("DS_Status");
+
             string queryRelatorio = "SELECT top 10 * FROM V_FISCAL ";
 
             string queryPV = "SELECT top 10 DocEntry, CardCode, CardName, DocDate, DocDueDate, DocTotal FROM ORDR ";
 
-            if (!string.IsNullOrEmpty(EditText0.Value))
+            if (!string.IsNullOrEmpty(txtCardCode.Value))
             {
-                queryRelatorio += (queryRelatorio.Contains("WHERE") ? "AND " : "WHERE ") + $"\"Codigo PN\" = '{EditText0.Value}'";
-                queryPV += (queryPV.Contains("WHERE") ? "AND " : "WHERE ") + $"\"CardCode\" = '{EditText0.Value}'";
+                queryRelatorio += (queryRelatorio.Contains("WHERE") ? "AND " : "WHERE ") + $"\"Codigo PN\" = '{txtCardCode.Value}'";
+                queryPV += (queryPV.Contains("WHERE") ? "AND " : "WHERE ") + $"\"CardCode\" = '{txtCardCode.Value}'";
             }
 
             if (!string.IsNullOrEmpty(EditText2.Value))
@@ -106,7 +126,7 @@ namespace SAPExemple
                 queryPV += (queryPV.Contains("WHERE") ? "AND " : "WHERE ") + $"\"DocDate\" <= '{EditText3.Value}'";
             }
 
-            Grid0.DataTable.ExecuteQuery(queryRelatorio);
+            gridQuery.DataTable.ExecuteQuery(queryRelatorio);
 
             Recordset oRecord = (Recordset)Program.oCompany.GetBusinessObject(BoObjectTypes.BoRecordset);
 
@@ -115,28 +135,49 @@ namespace SAPExemple
             int index = 0;
 
             UIAPIRawForm.Freeze(true);
-            ClearDataTable(Grid1.DataTable);
+            ClearDataTable(gridManual.DataTable);
             while (!oRecord.EoF)
             {
-                Grid1.DataTable.Rows.Add();
+                gridManual.DataTable.Rows.Add();
 
-                Grid1.DataTable.Columns.Item("Documento").Cells.Item(index).Value = oRecord.Fields.Item("DocEntry").Value.ToString();
-                Grid1.DataTable.Columns.Item("Cliente Código").Cells.Item(index).Value = oRecord.Fields.Item("CardCode").Value.ToString();
-                Grid1.DataTable.Columns.Item("Cliente Nome").Cells.Item(index).Value = oRecord.Fields.Item("CardName").Value.ToString();
-                Grid1.DataTable.Columns.Item("Data Documento").Cells.Item(index).Value = oRecord.Fields.Item("DocDate").Value;
-                Grid1.DataTable.Columns.Item("Data Vencimento").Cells.Item(index).Value = oRecord.Fields.Item("DocDueDate").Value;
-                Grid1.DataTable.Columns.Item("Total").Cells.Item(index).Value = oRecord.Fields.Item("DocTotal").Value;
+                gridManual.DataTable.Columns.Item("Documento").Cells.Item(index).Value = oRecord.Fields.Item("DocEntry").Value.ToString();
+                gridManual.DataTable.Columns.Item("Cliente Código").Cells.Item(index).Value = oRecord.Fields.Item("CardCode").Value.ToString();
+                gridManual.DataTable.Columns.Item("Cliente Nome").Cells.Item(index).Value = oRecord.Fields.Item("CardName").Value.ToString();
+                gridManual.DataTable.Columns.Item("Data Documento").Cells.Item(index).Value = oRecord.Fields.Item("DocDate").Value;
+                gridManual.DataTable.Columns.Item("Data Vencimento").Cells.Item(index).Value = oRecord.Fields.Item("DocDueDate").Value;
+                gridManual.DataTable.Columns.Item("Total").Cells.Item(index).Value = oRecord.Fields.Item("DocTotal").Value;
 
                 oRecord.MoveNext();
                 index++;
             }
             UIAPIRawForm.Freeze(false);
 
-            Grid0.AutoResizeColumns();
-            Grid1.AutoResizeColumns();
-        }
+            gridQuery.AutoResizeColumns();
+            gridManual.AutoResizeColumns();
 
-        private SAPbouiCOM.Matrix Matrix0;
+            dt_Matrix.ExecuteQuery("SELECT top 100 CardCode, DocDate, CONCAT(Min(DocDate), ' - ', Max(DocDate)) AS 'Periodo',(SUM(OI.DocTotal) / MONTH((Min(DocDate) - Max(DocDate)))) AS 'FatMes',(SUM(OI.DocTotal) / YEAR((Min(DocDate) - Max(DocDate)))) AS 'FatAno',SUM(OI.DocTotal) AS 'Total' FROM OINV OI GROUP BY CardCode, DocDate");
+
+            for (int i = 0; i < dt_Matrix.Rows.Count; i++)
+            {
+                dt_Status.Rows.Add();
+
+                dt_Status.Columns.Item("Status").Cells.Item(i).Value = i % 2 == 0 ? "Atrasada" : "Correta";
+            }
+
+            oMatrix.LoadFromDataSource();
+
+            for (int i = 0; i < oMatrix.RowCount; i++)
+            {
+                if (dt_Status.Columns.Item("Status").Cells.Item(i).Value.ToString() == "Atrasada")
+                {
+                    oMatrix.CommonSetting.SetCellBackColor(i + 1, 6, RGBtoInt(255, 160, 122));
+                }
+                else
+                {
+                    oMatrix.CommonSetting.SetCellBackColor(i + 1, 6, RGBtoInt(124, 252, 0));
+                }
+            }
+        }
 
         private void EditText0_ChooseFromListAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
         {
@@ -170,17 +211,6 @@ namespace SAPExemple
             }
         }
 
-        private StaticText StaticText2;
-        private StaticText StaticText3;
-        private StaticText StaticText4;
-        private EditText EditText2;
-        private EditText EditText3;
-        private StaticText StaticText5;
-        private StaticText StaticText6;
-        private OptionBtn OptionBtn0;
-        private OptionBtn OptionBtn1;
-        private OptionBtn OptionBtn2;
-
         private void OptionBtn0_ClickBefore(object sboObject, SBOItemEventArg pVal, out bool BubbleEvent)
         {
             BubbleEvent = true;
@@ -189,7 +219,7 @@ namespace SAPExemple
 
             string b = userDS.Value;
 
-            Grid1.CollapseLevel = 0;
+            gridManual.CollapseLevel = 0;
         }
 
         private void OptionBtn1_ClickBefore(object sboObject, SBOItemEventArg pVal, out bool BubbleEvent)
@@ -199,7 +229,7 @@ namespace SAPExemple
 
             string b = userDS.Value;
 
-            Grid1.CollapseLevel = 2;
+            gridManual.CollapseLevel = 2;
         }
 
         private void OptionBtn2_ClickBefore(object sboObject, SBOItemEventArg pVal, out bool BubbleEvent)
@@ -209,10 +239,14 @@ namespace SAPExemple
 
             string b = userDS.Value;
 
-            Grid1.CollapseLevel = 3;
+            gridManual.CollapseLevel = 3;
         }
 
-        private PictureBox PictureBox1;
-        private StaticText StaticText7;
+        public static int RGBtoInt(int r, int g, int b)
+        {
+            return (r << 0) | (g << 8) | (b << 16);
+        }
+
+
     }
 }
